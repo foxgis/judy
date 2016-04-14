25cb79df0be2b70747b2b70a2c4d98fe7334d70d
jingsam <jingsam@163.com>
2016-04-14 18:45:24 +0800

    add test

--- test/user.test.js
+++ test/user.test.js
@@ -1,12 +1,46 @@
 var app = require('../app')
 var request = require('supertest')
+var User = require('../models/user')
 
 
-describe('用户系统', function(){
-  it('登录', function(done){
+describe('用户系统', function() {
+
+  after('清除用户数据', function() {
+    User.remove({ username: 'jingsam' }).exec()
+  })
+
+  it('注册', function(done) {
+    request(app)
+      .post('/api/v1/users')
+      .send({ username: 'jingsam', password: '123456' })
+      .expect(200, done)
+  })
+
+  it('登录', function(done) {
     request(app)
       .post('/api/v1/users/jingsam')
-      .send({ username: 'jingsam', password: '123456'})
-      .expect(200, done);
+      .send({ username: 'jingsam', password: '123456' })
+      .expect(200, done)
+  })
+
+  it('获取用户信息', function(done) {
+    request(app)
+      .post('/api/v1/users/jingsam')
+      .send({ username: 'jingsam', password: '123456' })
+      .expect(200, done)
+  })
+
+  it('更新用户信息', function(done) {
+    request(app)
+      .post('/api/v1/users/jingsam')
+      .send({ username: 'jingsam', password: '123456' })
+      .expect(200, done)
+  })
+
+  it('获取access_token', function(done) {
+    request(app)
+      .post('/api/v1/users/jingsam')
+      .send({ username: 'jingsam', password: '123456' })
+      .expect(200, done)
   })
 })
