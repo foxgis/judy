ff78160d8f4d7e4dcc971f7ee4a7d250acac3192
kjdongzh <1044855318@qq.com>
2016-08-24 13:59:21 +0800

    modify

--- controllers/font.js
+++ controllers/font.js
@@ -59,8 +59,6 @@ module.exports.upload = function(req, res) {
       },
       writeDB: function(writePbf, writeFile, font, callback) {
         var newFont = {
-          createdAt: new Date(),
-          updatedAt: new Date(),
           fontname: font.name,
           owner: username,
           is_deleted: false,
