e98c987d4fbeddbdb18408fc519c49721be56d98
chenlisheng <335637257@qq.com>
2016-12-14 12:11:00 +0800

    add newsMap&mapCheck

---
+++ public/mapCheck/static/api.js
@@ -0,0 +1,11 @@
+var baseURL = 'http://192.9.105.205:8090/api/v1'
+window.SERVER_API = {
+  users: baseURL + '/users',
+  styles: baseURL + '/styles',
+  tilesets: baseURL + '/tilesets',
+  fonts: baseURL + '/fonts',
+  sprites: baseURL + '/sprites',
+  uploads: baseURL + '/uploads',
+  stats:baseURL + '/stats',
+  templates:baseURL + '/templates'
+}
